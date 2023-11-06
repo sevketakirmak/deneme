b2b1d195c56296be5c9421124f17ae77c1069a05
Şevket Akırmak <sevketakirmak3@gmail.com>
2023-11-06 12:31:20 +0300

    up

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,13 @@
 import { Inter } from "next/font/google";
-import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [isDatePickerActive, setDatePickerActive] = useState(false);
-
-  const handleFocus = () => {
-    setDatePickerActive(true);
-  };
-
-  const handleBlur = () => {
-    setDatePickerActive(false);
-  };
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <input
-        placeholder="Date"
-        className="textbox-n"
-        type={isDatePickerActive ? "date" : "text"}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        id="date"
-      />
+      <input type="date" className="appearance-none" placeholder="doğum tarihi" />
     </main>
   );
 }
